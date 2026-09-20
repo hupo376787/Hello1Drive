824829791db26c2a02bf0764bd8417d31af1e7fd
Vincent Wang <376787823@qq.com>
2026-09-20 09:46:38 +0800

    Reduce desktop thumbnail decode memory

--- src/Hello1Drive.Core/ViewModels/MainViewModel.cs
+++ src/Hello1Drive.Core/ViewModels/MainViewModel.cs
@@ -4806,7 +4806,7 @@ public partial class MainViewModel : ViewModelBase
             {
                 bitmap = await DecodeThumbnailBitmapAsync(
                     cachedPath,
-                    IsMobilePlatform ? 160 : 320,
+                    IsMobilePlatform ? 160 : 256,
                     cancellationToken).ConfigureAwait(false);
             }
             catch when (!cancellationToken.IsCancellationRequested)
@@ -4822,7 +4822,7 @@ public partial class MainViewModel : ViewModelBase
 
                 bitmap = await DecodeThumbnailBitmapAsync(
                     cachedPath,
-                    IsMobilePlatform ? 160 : 320,
+                    IsMobilePlatform ? 160 : 256,
                     cancellationToken).ConfigureAwait(false);
             }
 
