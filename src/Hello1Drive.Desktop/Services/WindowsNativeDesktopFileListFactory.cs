f3c1e9caff6a9b9fd2058f8d8712420f4dbf2d4d
Vincent Wang <376787823@qq.com>
2026-09-20 09:45:25 +0800

    Add native item state mask constant

--- src/Hello1Drive.Desktop/Services/WindowsNativeDesktopFileListFactory.cs
+++ src/Hello1Drive.Desktop/Services/WindowsNativeDesktopFileListFactory.cs
@@ -107,6 +107,7 @@ internal sealed partial class WindowsNativeDesktopFileListController : IDisposab
     private const uint LVIS_SELECTED = 0x0002;
     private const uint LVIF_TEXT = 0x0001;
     private const uint LVIF_IMAGE = 0x0002;
+    private const uint LVIF_STATE = 0x0008;
     private const uint LVCF_FMT = 0x0001;
     private const uint LVCF_WIDTH = 0x0002;
     private const uint LVCF_TEXT = 0x0004;
